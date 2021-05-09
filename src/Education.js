05a0beebea902b7db693f22ef5d990e019a32289
spaley <serena.paley8@gmail.com>
2021-05-08 21:09:07 -0500

    first commit

---
+++ src/Education.js
@@ -0,0 +1,15 @@
+import React from "react"; 
+import "./App.css";
+
+function Education() {
+return (
+    <dev className= "education">
+    <h1>Education</h1>
+    <h1>Tester</h1>
+    </dev>
+);
+
+
+}
+
+export default Education;
